3242ffabf010cd5a84f1d4b409404273863dff80
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-07-08 22:04:01 +0200

    Improve error handling and implement returning arbitrary json

--- src/api/stock/overview.rs
+++ src/api/stock/overview.rs
@@ -1,20 +1,60 @@
 // https://github.com/mistressofjellyfish/not-grocy/blob/ddc2dad07ec26f854cca78bbdbec92b2213ad235/php/Controllers/StockApiController.php#L332
 
+use std::fmt;
+use std::fmt::Debug;
+
 use crate::models::*;
-use actix_web::{get, web, Error, HttpResponse};
+use actix_web::{get, web, HttpResponse};
 use diesel::prelude::*;
 use diesel::r2d2::ConnectionManager;
 use diesel::sqlite::SqliteConnection;
 type DbPool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+use r2d2::PooledConnection;
+use serde::{Deserialize, Serialize};
 
-#[get("/api/stock/overview")]
-pub async fn index(pool: web::Data<DbPool>) -> Result<HttpResponse, Error> {
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct StockOverviewResponse {
+    current_stock: Vec<Stock>,
+    current_stock_locations: Vec<Stock>,
+}
+
+#[derive(Debug)]
+struct DieselError(diesel::result::Error);
+
+impl fmt::Display for DieselError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        std::fmt::Display::fmt(&self.0, f)
+    }
+}
+
+impl actix_web::error::ResponseError for DieselError {}
+
+fn action_stock_overview(
+    connection: PooledConnection<ConnectionManager<SqliteConnection>>,
+) -> QueryResult<StockOverviewResponse> {
     use crate::schema::stock::dsl::*;
+    Ok(StockOverviewResponse {
+        current_stock: stock.load::<Stock>(&connection)?,
+        current_stock_locations: stock.load::<Stock>(&connection)?,
+    })
+}
 
-    let connection = pool.get().expect("couldn't get db connection from pool");
+#[derive(Debug)]
+struct R2D2Error(r2d2::Error);
 
-    Ok(web::block(move || stock.load::<Stock>(&connection))
-        .await
-        .map(|the_stock| HttpResponse::Ok().json(the_stock))
-        .map_err(|_| HttpResponse::InternalServerError())?)
+impl fmt::Display for R2D2Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        std::fmt::Display::fmt(&self.0, f)
+    }
+}
+
+impl actix_web::error::ResponseError for R2D2Error {}
+
+// https://github.com/mistressofjellyfish/not-grocy/blob/ddc2dad07ec26f854cca78bbdbec92b2213ad235/php/Controllers/StockApiController.php#L332
+#[get("/api/stock/overview")]
+pub async fn index(pool: web::Data<DbPool>) -> actix_web::Result<HttpResponse> {
+    let connection = pool.get().map_err(R2D2Error)?;
+    Ok(HttpResponse::Ok()
+        .json(web::block(move || action_stock_overview(connection).map_err(DieselError)).await?))
 }
