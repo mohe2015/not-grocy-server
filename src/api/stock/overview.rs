0e899ecb4e6e68adab6cd90922b045bd6e7f874d
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-07-15 01:19:00 +0200

    another attempt

--- src/api/stock/overview.rs
+++ src/api/stock/overview.rs
@@ -2,11 +2,18 @@
 
 use std::fmt;
 use std::fmt::Debug;
+use std::str;
 
 use crate::models::*;
-use actix_web::{get, web, HttpResponse};
+use actix_web::{web, HttpResponse};
+use chrono::NaiveDate;
+use chrono::NaiveDateTime;
+use diesel::backend::UsesAnsiSavepointSyntax;
+use diesel::connection::AnsiTransactionManager;
 use diesel::prelude::*;
 use diesel::r2d2::ConnectionManager;
+use diesel::types::FromSql;
+use diesel::types::HasSqlType;
 use r2d2::PooledConnection;
 use serde::{Deserialize, Serialize};
 
@@ -28,9 +35,18 @@ impl fmt::Display for DieselError {
 
 impl actix_web::error::ResponseError for DieselError {}
 
-fn action_stock_overview<T: Connection + 'static>(
+fn action_stock_overview<T>(
     connection: PooledConnection<ConnectionManager<T>>,
-) -> QueryResult<StockOverviewResponse> {
+) -> QueryResult<StockOverviewResponse>
+where
+    T: Connection<TransactionManager = AnsiTransactionManager> + 'static,
+    NaiveDate: FromSql<diesel::sql_types::Date, <T as diesel::Connection>::Backend>,
+    <T as diesel::Connection>::Backend: HasSqlType<diesel::sql_types::Bool>,
+    <T as diesel::Connection>::Backend: UsesAnsiSavepointSyntax,
+    NaiveDateTime: FromSql<diesel::sql_types::Timestamp, <T as diesel::Connection>::Backend>,
+    bool: FromSql<diesel::sql_types::Bool, <T as diesel::Connection>::Backend>,
+    <T as diesel::Connection>::Backend: HasSqlType<diesel::sql_types::Text>,
+{
     use crate::schema::stock::dsl::*;
     Ok(StockOverviewResponse {
         current_stock: stock.load::<Stock>(&connection)?,
@@ -50,7 +66,17 @@ impl fmt::Display for R2D2Error {
 impl actix_web::error::ResponseError for R2D2Error {}
 
 // https://github.com/mistressofjellyfish/not-grocy/blob/ddc2dad07ec26f854cca78bbdbec92b2213ad235/php/Controllers/StockApiController.php#L332
-pub async fn index<T: Connection + 'static>(pool: web::Data<r2d2::Pool<ConnectionManager<T>>>) -> actix_web::Result<HttpResponse> {
+pub async fn index<T>(
+    pool: web::Data<r2d2::Pool<ConnectionManager<T>>>,
+) -> actix_web::Result<HttpResponse>
+where
+    T: Connection<TransactionManager = AnsiTransactionManager> + 'static,
+    NaiveDate: FromSql<diesel::sql_types::Date, <T as diesel::Connection>::Backend>,
+    <T as diesel::Connection>::Backend: HasSqlType<diesel::sql_types::Bool>,
+    <T as diesel::Connection>::Backend: UsesAnsiSavepointSyntax,
+    NaiveDateTime: FromSql<diesel::sql_types::Timestamp, <T as diesel::Connection>::Backend>,
+    bool: FromSql<diesel::sql_types::Bool, <T as diesel::Connection>::Backend>,
+{
     let connection = pool.get().map_err(R2D2Error)?;
     Ok(HttpResponse::Ok()
         .json(web::block(move || action_stock_overview(connection).map_err(DieselError)).await?))
--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,14 @@ pub mod models;
 pub mod schema;
 
 use actix_web::web;
-use diesel::Connection;
-use diesel::PgConnection;
+use chrono::{NaiveDate, NaiveDateTime};
+use diesel::backend::UsesAnsiSavepointSyntax;
+use diesel::connection::AnsiTransactionManager;
 use diesel::r2d2::ConnectionManager;
 use diesel::sqlite::SqliteConnection;
+use diesel::Connection;
+use diesel::PgConnection;
+use diesel::types::{FromSql, HasSqlType};
 use dotenv::dotenv;
 use r2d2::Pool;
 use std::env;
@@ -18,15 +22,24 @@ use std::env;
 use actix_web::{App, HttpServer};
 
 // https://stackoverflow.com/questions/65645622/how-do-i-pass-a-trait-as-application-data-to-actix-web
-async fn run<T: Connection + 'static>(manager: ConnectionManager<T>) -> std::io::Result<()> {
+async fn run<T>(manager: ConnectionManager<T>) -> std::io::Result<()>
+where
+    T: Connection<TransactionManager = AnsiTransactionManager> + 'static,
+    NaiveDate: FromSql<diesel::sql_types::Date, <T as diesel::Connection>::Backend>,
+    <T as diesel::Connection>::Backend: HasSqlType<diesel::sql_types::Bool>,
+    <T as diesel::Connection>::Backend: UsesAnsiSavepointSyntax,
+    NaiveDateTime: FromSql<diesel::sql_types::Timestamp, <T as diesel::Connection>::Backend>,
+    bool: FromSql<diesel::sql_types::Bool, <T as diesel::Connection>::Backend>,
+{
     let pool: Pool<ConnectionManager<T>> = r2d2::Pool::builder()
-    .build(manager)
-    .expect("Failed to create database connection pool.");
+        .build(manager)
+        .expect("Failed to create database connection pool.");
 
     HttpServer::new(move || {
-        App::new()
-            .data(pool.clone())
-            .route("/api/stock/overview", web::get().to(api::stock::overview::index::<T>))
+        App::new().data(pool.clone()).route(
+            "/api/stock/overview",
+            web::get().to(api::stock::overview::index::<T>),
+        )
     })
     .bind("127.0.0.1:8080")?
     .run()
@@ -36,7 +49,7 @@ async fn run<T: Connection + 'static>(manager: ConnectionManager<T>) -> std::io:
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-    
+
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     let database_type = 1;
