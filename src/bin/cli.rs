f76486917c77c315099cf00ccd2828cd8dee5d00
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-07-18 00:12:54 +0200

    Implement migration cli interface

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -14,6 +14,15 @@ use diesel_migrations::Migration;
 use diesel_migrations::MigrationConnection;
 use dotenv::dotenv;
 use migrations_internals::schema::__diesel_schema_migrations::dsl::*;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(about = "the not-grocy database migration tool")]
+enum Cli {
+    Migrate,
+    ListMigrations,
+    Rollback { version: String },
+}
 
 pub struct BarrelMigration {
     version: String,
@@ -41,7 +50,12 @@ impl Migration for BarrelMigration {
     }
 }
 
+// we roll our own cli because the official one creates terrible errors if the migrations have compilation errors
+// and developing migrations has no good ide support.
+// also switching databases is not supported.
+
 fn main() -> Result<(), RunMigrationsError> {
+    let args = Cli::from_args();
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let connection = SqliteConnection::establish(&database_url)
@@ -49,10 +63,8 @@ fn main() -> Result<(), RunMigrationsError> {
 
     let mut m_up = barrel::Migration::new();
     not_grocy_server::migrations::m20210716230021_init::up(&mut m_up);
-    println!("{}", m_up.make::<Sqlite>());
     let mut m_down = barrel::Migration::new();
     not_grocy_server::migrations::m20210716230021_init::down(&mut m_down);
-    println!("{}", m_down.make::<Sqlite>());
 
     let migration = BarrelMigration {
         version: "20210716230021".to_string(),
@@ -60,23 +72,27 @@ fn main() -> Result<(), RunMigrationsError> {
         down: m_down.make::<Sqlite>(),
     };
 
-    let migration_to_revert = BarrelMigration {
-        version: "20210716230021".to_string(),
-        up: m_up.make::<Sqlite>(),
-        down: m_down.make::<Sqlite>(),
-    };
-
     let migrations = [migration];
-
     println!("{:?}", connection.latest_run_migration_version()?);
 
-    /*connection.transaction::<_, RunMigrationsError, _>(|| {
-        println!("Rolling back migration {}", migration_to_revert.version());
-        migration_to_revert.revert(&connection)?;
-        let target = __diesel_schema_migrations.filter(version.eq(migration_to_revert.version()));
-        ::diesel::delete(target).execute(&connection)?;
-        Ok(())
-    })?;*/
+    match args {
+        Cli::Migrate => run_migrations(&connection, migrations, &mut std::io::stdout()),
+        Cli::ListMigrations => Ok(()),
+        Cli::Rollback { version: v } => {
+            let migration_to_revert = BarrelMigration {
+                version: "20210716230021".to_string(),
+                up: m_up.make::<Sqlite>(),
+                down: m_down.make::<Sqlite>(),
+            };
 
-    run_migrations(&connection, migrations, &mut std::io::stdout())
+            connection.transaction::<_, RunMigrationsError, _>(|| {
+                println!("Rolling back migration {}", migration_to_revert.version());
+                migration_to_revert.revert(&connection)?;
+                let target =
+                    __diesel_schema_migrations.filter(version.eq(migration_to_revert.version()));
+                ::diesel::delete(target).execute(&connection)?;
+                Ok(())
+            })
+        }
+    }
 }
